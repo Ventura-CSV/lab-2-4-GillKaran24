43c19d8a7c82161b795e17831025571922b2f85f
Karan <karangill2497@gmail.com>
2023-11-29 20:19:42 -0800

    lab 2-4 done

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    original_str = "Python Programming"
 
+    # Extract "Python" from the original string with index slicing
+    sub1 = original_str[:6]
+
+    # Extract "Programming" from the original string with index slicing
+    sub2 = original_str[7:]
+
+    # Merge two substrings using string concatenation
+    merged_str = sub2 + " " + sub1
+
+    print(sub2)
+    print(sub1)
+    print(merged_str)
+
+    # Do not delete the return statement
+    return sub2, sub1, merged_str
 
 if __name__ == '__main__':
     main()
